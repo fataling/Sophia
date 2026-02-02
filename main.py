08dcfb1b19e5bb0a58b7effd48590a82a46b2951
if you dont do it <dontlistenthem@protonmail.com>
2026-02-02 23:54:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-from asyncio.exceptions import CancelledError
-from routers.main_routers import main_router
 from aiogram import Dispatcher, Bot
-from tokens import api_key
-from others.cfg import log
 
-from database.db import connector_to_server, create_database, create_table
+from database.db import create_database, create_table, cnn_pool
+from routers.main_routers import main_router
+from others.cfg import log
+from tokens import api_key
 
 import asyncio
-
-bot = Bot(token=api_key)
 
 dspt = Dispatcher()
 dspt.include_router(
     main_router
-)
+)   
 
 async def main():
-    connect = await connector_to_server()
-    await create_database(connect)
-    await create_table(connect)
+    pool = await cnn_pool()
+    await create_database(pool)
+    await create_table(pool)
     try:
+        bot = Bot(
+            token=api_key
+            )
         await dspt.start_polling(bot)
     except KeyboardInterrupt:
-        log('Code was completed!')
+        await pool.close()
+        await pool.wait_closed()
+        log('Code was completed!') 
     
 if __name__ == "__main__":
     asyncio.run(main())
